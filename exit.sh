0d8b0629c013d5109711c63c73e326788efcbcc9
devilatha <latha.korm@gmail.com>
2023-09-09 13:51:39 +0530

    sample script

--- exit.sh
+++ exit.sh
@@ -2,11 +2,11 @@ echo to check program status
 lss -ltr
 if [$? -ne 0] ; then
   echo it's failure
-  exit1
+  exit 1
 fi
 lss -ltr
 if [$? -ne 0] ; then
    echo it's failure
-   exit1
+   exit 1
 fi
  echo prog sucess
