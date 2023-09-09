b9480bf234d197c08082875f1fe912e4f99dbd17
devilatha <latha.korm@gmail.com>
2023-09-09 12:50:39 +0530

    sample script

--- hello.sh
+++ hello.sh
@@ -1,9 +1,8 @@
 echo "hello world"
 P1=$1
 P2=$2
-P3=${date}
 echo HI $P1
 echo Hi $P2
 echo HOw are you $P1
 echo i am fine what about you $P2
-echo script excuted at : $P3
+echo script excuted at : $date
